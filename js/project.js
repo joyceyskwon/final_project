7f7b64a6a2132bebee97f36ce1f920df2dc650db
joycekwon <joyceyskwon@gmail.com>
2016-08-25 18:19:59 -0400

    changes in CSS and HTML
    Making structure, grid, hover system

---
+++ js/project.js
@@ -0,0 +1,5 @@
+ $(document).ready(function(){
+ 	$(".header_image").mouseenter(function(){
+ 		$(".header_image").addClass("header_iamge_hover", 100).addClass("header_image_clicked". 100);
+ 	});
+ });
