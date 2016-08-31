8f1c636b90f401a0c552f7b1e846bb45cce61d85
joycekwon <joyceyskwon@gmail.com>
2016-08-30 21:01:44 -0400

    final stretch
    !!!!

--- js/project.js
+++ js/project.js
@@ -1,5 +1,6 @@
  $(document).ready(function(){
- 	/*$(".header_image").mouseenter(function(){
- 		$(".header_image").addClass("header_iamge_hover", 100).addClass("header_image_clicked". 100);
- 	});*/
+ 	$("#hamburger").click(function() {
+ 		$("nav a").toggle();
+ 		$("#hamburger").show();
+ 	});
  });
